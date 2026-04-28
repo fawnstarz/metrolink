de7421c03ac966f41d41bf551259892c5b0f75d1
aimee <champ4gnec0ast@gmail.com>
2026-04-28 23:54:57 +0100

    issues with naming conventions, attempting to fix them to prevent future errors

--- src/gui/input.java
+++ src/gui/input.java
@@ -1,5 +1,17 @@
 package gui;
 
-public class input {
+// imports from swing for necessary components
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+// utilising jpanel for input field for destination and arrival locations, alongside check button & result
+public class input extends JPanel{
+    public Input(Stations stations) {
+        JTextField departure = new JTextField(20);
+        JTextField arrival = new JTextField(20);
+        JButton check = new JButton("Check");
+        JLabel result = new JLabel(" ");
+    }
 }
