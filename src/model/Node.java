638911eeb931fde47286285db736e78ed9d24e94
aimee <champ4gnec0ast@gmail.com>
2026-05-01 15:32:48 +0100

    TASK ONE: adding interactivity for users, allowing submissions and checking of inputted stations + fixing slight logic issues

--- src/model/Node.java
+++ src/model/Node.java
@@ -1,5 +1,12 @@
 package model;
 
 public class Node {
+    // name string
+    // minimum distance of node
+    // map of adjacent nodes -> consider the line it runs on
+    // list -> shortest path from the source with each line
+
+    // add adjacent node method 
+    // compare to method
 
 }
---
+++ src/model/WeightedGraph.java
@@ -0,0 +1,8 @@
+package model;
+
+public class WeightedGraph {
+    // hash map to form base of adjacency matrix
+
+    
+
+}
